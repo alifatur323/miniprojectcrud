e3d65f444514993d29ea27574a029e18f3b39d1b
alifatur <alifatur323@gmail.com>
2023-06-07 19:12:28 +0700

    feat: crud customer, crud actor

---
+++ modules/account/router.go
@@ -0,0 +1,24 @@
+package account
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type RouterActor struct {
+	ActorRequestHandler ActorStructRequestHandler
+}
+
+func NewRouter(dbCrud *gorm.DB) RouterActor {
+	return RouterActor{ActorRequestHandler: NewActorRequestHandler(dbCrud)}
+}
+
+func (r RouterActor) Handle(router *gin.Engine) {
+	basepath := "/account"
+	account := router.Group(basepath)
+	account.POST("/login", r.ActorRequestHandler.LoginActor)
+	account.POST("/", r.ActorRequestHandler.CreateActor)
+	account.GET("/:id", r.ActorRequestHandler.GetActorById)
+	account.PUT("/:id", r.ActorRequestHandler.UpdateActor)
+	account.DELETE("/:id", r.ActorRequestHandler.DeleteActor)
+}
